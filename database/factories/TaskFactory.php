c184f8e3884363eafb3e2026e0bf1ff3fa9c0344
yudwig <yudwig.dev@gmail.com>
2020-06-19 09:17:31 +0900

    Change directory structure.

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Task::class, function (Faker $faker, $attributes) {
+    return [
+        'title' => $faker->sentence(2),
+        'completed' => 0,
+        'category_id' => $attributes['category_id']
+    ];
+});
